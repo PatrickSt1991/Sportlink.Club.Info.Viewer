b38f41d4dd9c504411466fafa8ea1f9202abf6a2
PatrickSt1991 <patrick.stel@kpnmail.nl>
2025-06-21 15:57:32 +0200

    fixes routing path issue #36

--- src/components/ConfigSettings.vue
+++ src/components/ConfigSettings.vue
@@ -85,7 +85,7 @@
       <div class="form-group">
         <label class="leftLabel">Start scherm:</label>
         <select v-model="localConfig.homeScreen">
-          <option v-for="(path, label) in homeScreens" :key="label" :value="label">
+          <option v-for="(path, label) in homeScreens" :key="label" :value="path">
             {{ label }}
           </option>
         </select>
--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const routes = [
   },
   {
     path: '/:pathMatch(.*)*',
-    redirect: () => HOME_SCREENS[USER_CONFIG.homeScreen] || '/match-info',
+    redirect: () => USER_CONFIG.value.homeScreen || '/match-info',
   },
 ];
 
